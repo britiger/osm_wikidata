4dc046d08828391192ee2d4b19435f26b67acadf
Christopher Lorenz <britiger@gmx.net>
2020-07-26 16:55:56 +0200

    Rename Scripts and some output at wikidata script

--- loadWikidata.sh
+++ loadWikidata.sh
@@ -7,15 +7,19 @@ wd_cache=./cache_wikidata
 
 mkdir -p $wd_cache
 
+echo_time "Check tables ..."
 psql -f sql/createWikiTables.sql > /dev/null
+psql -f sql/classifyWikidata.sql > /dev/null
 
 function db_import() {
     wikidata=$1
     cat $wd_cache/${wikidata}.json | sed 's/\\/\\\\/g' | psql -c 'COPY wikidata_import FROM STDIN;'
 }
 
+echo_time "Cleanup tables ..."
 psql -c "TRUNCATE TABLE wikidata_import;"
 
+echo_time "Search for missing data ..."
 psql -t -X --quiet --no-align -c "select wikidata FROM wikidata_needed_import ORDER BY wikidata" \
 | while read wikidata
 do
@@ -51,3 +55,5 @@ SELECT jsonb_object_keys(data->'entities') AS wikidataId, data->'entities'->json
 FROM wikidata_import
 ON CONFLICT (wikidataId) DO UPDATE 
 SET imported=NOW(), data=excluded.data;"
+echo_time "Link entities ..."
+psql -f sql/classifyFunction.sql > /dev/null
