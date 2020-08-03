1e32b12125a013cee0cb72ef145a47881ec72df7
Christopher Lorenz <britiger@gmx.net>
2020-08-03 18:35:48 +0200

    rerun for deps + check state of loaded entity

--- loadWikidata.sh
+++ loadWikidata.sh
@@ -16,46 +16,80 @@ function db_import() {
     cat $wd_cache/${wikidata}.json | sed 's/\\/\\\\/g' | psql -c 'COPY wikidata_import FROM STDIN;'
 }
 
-echo_time "Cleanup tables ..."
-psql -c "TRUNCATE TABLE wikidata_import;"
+function import_run() {
+    echo_time "Cleanup tables ..."
+    psql -c "TRUNCATE TABLE wikidata_import;"
 
-echo_time "Search for missing data ..."
-psql -t -X --quiet --no-align -c "select wikidata FROM wikidata_needed_import ORDER BY wikidata" \
-| while read wikidata
-do
-    # regex check
-    if ! [[ $wikidata =~ ^Q[0-9]+$ ]]
-    then
-        echo_time "'${wikidata}' is not a valid wikidata entity! Skipped."
-        continue
-    fi
-
-    echo_time "Check ${wikidata} ..."
-    outfile=$wd_cache/${wikidata}.json
-    if ! [ -f $outfile ]
-    then
-        echo_time "Download ..."
-        curl -s https://www.wikidata.org/wiki/Special:EntityData/${wikidata}.json -o $outfile
-        # TODO: Check for: Rediects
-        # TODO: Check for: 404
-        RET=$?
-        if [ $RET -ne 0 ]
+    echo_time "Search for missing data ..."
+    psql -t -X --quiet --no-align -c "select wikidata FROM wikidata_needed_import ORDER BY wikidata" \
+    | while read wikidata
+    do
+        # regex check
+        if ! [[ $wikidata =~ ^Q[0-9]+$ ]]
         then
-            rm -rf $outfile
+            echo_time "'${wikidata}' is not a valid wikidata entity! Skipped."
+            continue
         fi
-    fi
-    if [ -f $outfile ]
+
+        echo_time "Check ${wikidata} ..."
+        outfile=$wd_cache/${wikidata}.json
+        if ! [ -f $outfile ]
+        then
+            echo_time "Download ..."
+            curl -s https://www.wikidata.org/wiki/Special:EntityData/${wikidata}.json -o $outfile
+            RET=$?
+            if [ $RET -ne 0 ]
+            then
+                rm -f $outfile
+            fi
+        fi
+        if [ -f $outfile ]
+        then
+            # Check for: Rediects
+            file_id=`jq -r '.entities | keys[0]' $outfile 2> /dev/null`
+            file_id_ret=$?
+            if [ $file_id_ret -ne 0 ]
+            then
+                if grep -q "Not Found" $outfile
+                then
+                    echo_time "Entity $wikidata not found, maybe deleted or invalid"
+                fi
+                echo_time "Reset cache"
+                rm -f $outfile
+                continue
+            fi
+            if [ "$file_id" != "$wikidata" ]
+            then
+                echo_time "Found Redirect $wikidata => $file_id"
+                rm -f $outfile
+                continue
+            fi
+            echo_time "Import to db ..."
+            db_import ${wikidata}
+        fi
+    done
+
+    echo_time "Finish Import ..."
+    psql -c "INSERT INTO wikidata (wikidataId, data)
+    SELECT jsonb_object_keys(data->'entities') AS wikidataId, data->'entities'->jsonb_object_keys(data->'entities') AS data
+    FROM wikidata_import
+    ON CONFLICT (wikidataId) DO UPDATE 
+    SET imported=NOW(), data=excluded.data;"
+
+    results=`psql -t -X --quiet --no-align -c "SELECT count(*) FROM wikidata_import"`
+    return $results
+}
+
+RET=1
+while [ "$RET" -ne 0 ]
+do
+    import_run
+    RET=$?
+    if [ "$RET" -ne 0 ]
     then
-        echo_time "Import to db ..."
-        db_import ${wikidata}
+        echo_time "Rerun for dependencies"
     fi
 done
 
-echo_time "Finish Import ..."
-psql -c "INSERT INTO wikidata (wikidataId, data)
-SELECT jsonb_object_keys(data->'entities') AS wikidataId, data->'entities'->jsonb_object_keys(data->'entities') AS data
-FROM wikidata_import
-ON CONFLICT (wikidataId) DO UPDATE 
-SET imported=NOW(), data=excluded.data;"
 echo_time "Link entities ..."
 psql -f sql/classifyFunction.sql > /dev/null
